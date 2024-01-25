5274ec02f3e17f59f2869adebee3e87f25ad2c22
TrujiDev <eetgrisales@gmail.com>
2024-01-25 10:56:19 -0500

    Refactor registerUser method in AuthController

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain';
+import { AuthService } from '../services/auth.service';
 
 export class AuthController {
-	constructor() {}
+	constructor(public readonly authService: AuthService) {}
 
 	loginUser = (req: Request, res: Response) => {
 		res.json({ message: 'login' });
 	};
 
 	registerUser = (req: Request, res: Response) => {
-		res.json({ message: 'register' });
+		const [error, registerDto] = RegisterUserDto.create(req.body);
+		if (error) return res.status(400).json({ message: error });
+
+		this.authService.registerUser(registerDto!)
+			.then(user => res.json(user))
 	};
 
 	validateEmail = (req: Request, res: Response) => {
