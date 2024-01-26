6549606f7ddab10f5b67534e59515e73f7bec6b5
TrujiDev <eetgrisales@gmail.com>
2024-01-26 11:23:07 -0500

    Update AuthController to include LoginUserDto in loginUser method

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { CustomError, RegisterUserDto } from '../../domain';
+import { CustomError, LoginUserDto, RegisterUserDto } from '../../domain';
 import { AuthService } from '../services/auth.service';
 
 export class AuthController {
@@ -15,14 +15,21 @@ export class AuthController {
 	};
 
 	loginUser = (req: Request, res: Response) => {
-		res.json({ message: 'login' });
+		const [error, loginDto] = LoginUserDto.create(req.body);
+		if (error) return res.status(400).json({ message: error });
+
+		this.authService
+			.loginUser(loginDto!)
+			.then(user => res.json(user))
+			.catch(error => this.handleError(error, res));
 	};
 
 	registerUser = (req: Request, res: Response) => {
 		const [error, registerDto] = RegisterUserDto.create(req.body);
 		if (error) return res.status(400).json({ message: error });
 
-		this.authService.registerUser(registerDto!)
+		this.authService
+			.registerUser(registerDto!)
 			.then(user => res.json(user))
 			.catch(error => this.handleError(error, res));
 	};
