51774325840fd72bcd4e7ea950e769c570b8da89
TrujiDev <eetgrisales@gmail.com>
2024-01-25 21:47:51 -0500

    Add error handling in AuthController

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
-import { RegisterUserDto } from '../../domain';
+import { CustomError, RegisterUserDto } from '../../domain';
 import { AuthService } from '../services/auth.service';
 
 export class AuthController {
 	constructor(public readonly authService: AuthService) {}
+
+	private handleError = (error: Error, res: Response) => {
+		if (error instanceof CustomError) {
+			return res.status(error.statusCode).json({ error: error.message });
+		}
+
+		console.log(`${error}`);
+		return res.status(500).json({ error: error.message });
+	};
 
 	loginUser = (req: Request, res: Response) => {
 		res.json({ message: 'login' });
@@ -15,6 +24,7 @@ export class AuthController {
 
 		this.authService.registerUser(registerDto!)
 			.then(user => res.json(user))
+			.catch(error => this.handleError(error, res));
 	};
 
 	validateEmail = (req: Request, res: Response) => {
