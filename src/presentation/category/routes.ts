260f67645350f6846bd3bd2b133c920a48925b43
TrujiDev <eetgrisales@gmail.com>
2024-01-31 13:06:30 -0500

    Add authentication middleware to category routes

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
 
 export class CategoryRoutes {
 	static get routes(): Router {
@@ -8,7 +9,11 @@ export class CategoryRoutes {
 		const categoryController = new CategoryController();
 
 		router.post('/', categoryController.createCategory);
-		router.get('/', categoryController.getCategories);
+		router.get(
+			'/',
+			[AuthMiddleware.validateJwt],
+			categoryController.getCategories
+		);
 
 		return router;
 	}
