29c57461f9998ad12164fbb94f6edeb89ec45e52
TrujiDev <eetgrisales@gmail.com>
2024-02-03 17:14:12 -0500

    Refactor product controller methods

--- src/presentation/product/controller.ts
+++ src/presentation/product/controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import { ProductService } from '../services/product.service';
 import { CreateProductDto, CustomError, PaginationDto } from '../../domain';
+import { ProductService } from '../services/product.service';
 
 export class ProductController {
 	constructor(private readonly productService: ProductService) {}
@@ -14,18 +14,7 @@ export class ProductController {
 		return res.status(500).json({ error: 'Internal server error' });
 	};
 
-	getAll(req: Request, res: Response) {
-		const { page = 1, limit = 10 } = req.query;
-		const [error, paginationDto] = PaginationDto.create(+page, +limit);
-		if (error) res.status(400).json({ error });
-
-		this.productService
-			.getAll(paginationDto!)
-			.then(products => res.status(200).json(products))
-			.catch(error => this.handleError(error, res));
-	}
-
-	create(req: Request, res: Response) {
+	create = (req: Request, res: Response) => {
 		const [error, createProductDto] = CreateProductDto.create({
 			...req.body,
 			user: req.body.user.id,
@@ -33,8 +22,19 @@ export class ProductController {
 		if (error) return res.status(400).json({ error });
 
 		this.productService
-			.create(createProductDto!)
+			.createProduct(createProductDto!)
 			.then(product => res.status(201).json(product))
 			.catch(error => this.handleError(error, res));
-	}
+	};
+
+	getAll = async (req: Request, res: Response) => {
+		const { page = 1, limit = 10 } = req.query;
+		const [error, paginationDto] = PaginationDto.create(+page, +limit);
+		if (error) return res.status(400).json({ error });
+
+		this.productService
+			.getProducts(paginationDto!)
+			.then(products => res.json(products))
+			.catch(error => this.handleError(error, res));
+	};
 }
