dd7a95b6efa41ca7789a664b773b8efd163dfce8
TrujiDev <eetgrisales@gmail.com>
2024-02-03 17:14:18 -0500

    Refactor product service methods

--- src/presentation/services/product.service.ts
+++ src/presentation/services/product.service.ts
@@ -3,32 +3,8 @@ import { CreateProductDto, CustomError, PaginationDto } from '../../domain';
 
 export class ProductService {
 	constructor() {}
-	async getAll(paginationDto: PaginationDto) {
-		const { page, limit } = paginationDto;
 
-		try {
-			const [products, total] = await Promise.all([
-				ProductModel.countDocuments(),
-				ProductModel.find()
-					.skip((page - 1) * limit)
-					.limit(limit),
-			]);
-
-			return {
-				page,
-				limit,
-				total,
-				next: `api/products?page=${page + 1}&limit=${limit}`,
-				prev:
-					(page - 1) * limit > 0
-						? `api/products?page=${page - 1}&limit=${limit}`
-						: null,
-				products,
-			};
-		} catch (error) {}
-	}
-
-	async create(createProductDto: CreateProductDto) {
+	async createProduct(createProductDto: CreateProductDto) {
 		const productExists = await ProductModel.findOne({
 			name: createProductDto.name,
 		});
@@ -42,6 +18,33 @@ export class ProductService {
 			return product;
 		} catch (error) {
 			throw CustomError.internalServer(`${error}`);
+		}
+	}
+
+	async getProducts(paginationDto: PaginationDto) {
+		const { page, limit } = paginationDto;
+
+		try {
+			const [total, products] = await Promise.all([
+				ProductModel.countDocuments(),
+				ProductModel.find()
+					.skip((page - 1) * limit)
+					.limit(limit)
+					.populate('user')
+					.populate('category'),
+			]);
+
+			return {
+				page: page,
+				limit: limit,
+				total: total,
+				next: `/api/products?page=${page + 1}&limit=${limit}`,
+				prev: page - 1 > 0 ? `/api/products?page=${page - 1}&limit=${limit}` : null,
+
+				products: products,
+			};
+		} catch (error) {
+			throw CustomError.internalServer('Internal Server Error');
 		}
 	}
 }
