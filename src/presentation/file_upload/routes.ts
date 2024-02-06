b45a66c16ebf0db6578646e59f43b7fd35391e10
TrujiDev <eetgrisales@gmail.com>
2024-02-06 15:27:02 -0500

    Add FileUploadService dependency to FileUploadController

--- src/presentation/file_upload/routes.ts
+++ src/presentation/file_upload/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { FileUploadController } from './controller';
+import { FileUploadService } from '../services/file_upload.service';
 
 export class FileUploadRoutes {
 	static get routes(): Router {
 		const router = Router();
 
-		const fileUploadController = new FileUploadController();
+		const fileUploadService = new FileUploadService();
+		const fileUploadController = new FileUploadController(fileUploadService);
 
 		router.post('/single/:type', fileUploadController.uploadFile);
 		router.post('/multiple/:type', fileUploadController.uploadMultipleFiles);
